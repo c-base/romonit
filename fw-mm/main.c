ca528bc052cce09e11092d45ac8e842dee2e1538
MM <mm@c-base.org>
2018-10-15 17:13:09 +0200

    adjust to 8s wakeup, more powersaving, disable button

--- fw-mm/main.c
+++ fw-mm/main.c
@@ -74,52 +74,129 @@ void set_display(void) {
 
 int main (void) {
 	uint32_t lcd_state = 43200;
+	uint32_t last = 0;
 	uint8_t	 button_down = 0;
 
-	_delay_ms(1);			// wait some time for things to settle
+	// wait some time for things to settle
+	_delay_ms(1);
 
-	// set clock to 4 Mhz
-	//CLKPR = (1<<CLKPCE);	// prescaler change enable
-	//CLKPR = (1<<CLKPS0);	// prescaler = 2 (internal RC runs @ 8MHz)
+	// disable watchdog
+	WDTCR |= (1<<WDCE) | (1<<WDE);
+	WDTCR = 0x00;
 
 	// disable analog comparator (saves power)
 	ACSR = (1<<ACD);
 
+	// disable JTAG  (needs to be written twice in 4 cycles)
+	MCUCR |= (1<<JTD);
+	MCUCR |= (1<<JTD);
+
 	// disable digital buffers on analog comparator
 	DIDR1 = 0x03;
+	// disable digital buffers on adc
+	DIDR0 = 0xff;
 
 	// switch on pullups according to data sheet
-	PORTA=0x0c;
-	PORTB=0x01;
-	////PORTC=0xff;
-	////PORTD=0xff;
-	PORTE=0x3f;
-	PORTF=0xff;
-	PORTG=0x36;
+	// PORTA (LCD)
+	// 0: COM0  (to LCD)
+	// 1: COM1  (to LCD)
+	// 2: COM2  unused
+	// 3: COM3  unused
+	// 4: SEG00 (to LCD)
+	// 5: SEG01 (to LCD)
+	// 6: SEG02 (to LCD)
+	// 7: SEG03 (to LCD)
+	PORTA=0xff;
+
+	// PORTB (SPI, OCxx)
+	// 0: SS
+	// 1: SCK
+	// 2: MOSI
+	// 3: MISO
+	// 4: OC0A (8 uA if pullup on)
+	// 5: OC1A (8 uA if pullup on)
+	// 6: OC1B (8 uA if pullup on)
+	// 7: OC2A (8 uA if pullup on)
+	PORTB=0x06;
+
+	// PORTC (LCD)
+	// 0: SEG12 unused
+	// 1: SEG11 unused
+	// 2: SEG10 unused
+	// 3: SEG09 unused
+	// 4: SEG08 unused
+	// 5: SEG07 (to LCD)
+	// 6: SEG06 (to LCD)
+	// 7: SEG05 (to LCD)
+	PORTC=0xff;
+
+	// PORTD (LCD)
+	// 0: SEG22 (to LCD)
+	// 1: SEG21 (to LCD)
+	// 2: SEG20 (to LCD)
+	// 3: SEG19 (to LCD)
+	// 4: SEG18 (to LCD)
+	// 5: SEG17 (to LCD)
+	// 6: SEG16 (to LCD)
+	// 7: SEG15 (to LCD)
+	PORTD=0xff;
+
+	// PORTE (TWI, SERIAL, analog Comperator)
+	// 0: RXD (to shrouded header)
+	// 1: TXD (to shrouded header)
+	// 2: XCK (to shrouded header)
+	// 3: AIN1 (to shrouded header)
+	// 4: USCK (to shrouded header)
+	// 5: SDA (to shrouded header)
+	// 6: DO  (to SHT SCK)
+	// 7: CLKO to SHT DATA)
+	PORTE=0xff;
+
+	// PORTF (0-7, ADC, JTAG)
+	// 0: ADC0 to shrouded header
+	// 1: ADC1 somehow related to LCD, pullup needs to be on (as output or as pullup)
+	// 2: ADC2 somehow related to SW1 (8uA when pressed and PF3 is high)
+	// 3: ADC3 to button SW1 ( 100uA when pressed and pullup is on)
+	// 4: ADC4 TCK to edge connector
+	// 5: ADC5 TMS to edge connector
+	// 6: ADC6 TDO to edge connector
+	// 7: ADC7 TDI to edge connector
+	PORTF=0xf3;
+	DDRF |= 2;
+
+	// PORTG (0-5)
+	// 0: SEG14 unused
+	// 1: SEG13 unused
+	// 2: SEG4  unused
+	// 3: SEG24 unused
+	// 4: SEG23 unused
+	// 5: RESET
+	PORTG=0x3f;
 
 	// power reduction mode for timer1, SPI, UART and ADC
 	PRR = (1<<PRLCD)|(1<<PRTIM1)|(1<<PRSPI)|(1<<PRUSART0)|(1<<PRADC);
 
-	led_init();
-	led_on(); _delay_ms(100); led_off();
+	//led_init();
+	//led_on(); _delay_ms(100); led_off();
 	timer2_init();
 	sht_init();
 	bat_update();
 	sei();
 	lcd_init();
 	sht_start();
-	sleep();
-	_delay_ms(1000);
+	last = sec;
+	while ( last == sec )
+		sleep();
 	sht_start();
-	sleep();
-	_delay_ms(1000);
-	sht_start();
-	sleep();
+	last = sec;
+		while ( last == sec )
+		sleep();
 	set_display();
 	lcd_update(0);
 
 	while (1) {
 		sleep();
+		#if 0
 		if ( button()) {
 			if ( !button_down ) {
 				if (lcd_state) {
@@ -139,26 +216,26 @@ int main (void) {
 			if ( button_down) 
 				button_down = 0;
 		}
+		#else
+			lcd_state = sec + 43200; /// hack always on
+		#endif
 		// blink the LED for 10ms every wakeup
-		//if ( (sec%10) == 0) led_on(); _delay_ms(1); led_off();
-		if (lcd_state) {
-			if ( lcd_state < sec ) {
-				lcd_off();
-				lcd_state = 0;
-			} else {
-				if (sec % 60 == 0) {
+		//if ( (sec%10) == 0) led_on(); _delay_ms(10); led_off();
+		#if 1
+		if (last != sec) {
+				last = sec;
+				//bat_update();
+				if (sec % 8 == 0) {
 					bat_update();
 					sht_start();
 					sleep();
 					set_display();
 				}
-				if ( sec % 4 == 2 ) {
+				if ( sec % 2 == 1 )
 					lcd_update(1);
-				} else if ( sec % 4 == 0 ) {
+				else
 					lcd_update(0);
-				}
-			}	
 		}
+		#endif
 	}
 }
-
